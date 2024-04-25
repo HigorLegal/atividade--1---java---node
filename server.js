f77740bf9b2c52dc74538991ae502debdf09d14c
HigorLegal <146496157+HigorLegal@users.noreply.github.com>
2024-04-25 20:09:19 -0300

    .

--- server.js
+++ server.js
@@ -1,26 +1,20 @@
-// Importa o Express
+
 const express = require('express');
 
-// Cria uma aplicação Express
 const app = express();
 
-// Middleware para analisar JSON
 app.use(express.json());
 
-// Middleware para dados de formulário
 app.use(express.urlencoded({ extended: true }));
 
-// Servindo arquivos estáticos
 app.use(express.static('public'));
 
-// Rota de login básica (GET)
 app.get('/login', (req, res) => {
   res.sendFile('index.html', { root: './public' });
 });
 
-// Simples autenticação de login (POST)
 app.post('/login', (req, res) => {
-  const { username, password } = req.body; // Aqui você implementará a lógica de validação
+  const { username, password } = req.body;
   if (username === "login" && password === "123") {
     res.send("Login bem-sucedido!");
   } else {
@@ -28,4 +22,5 @@ app.post('/login', (req, res) => {
   }
 });
  const PORT=3000
+
 app.listen(PORT, () => console.log(`Server rodando em http://localhost:${PORT}`));
